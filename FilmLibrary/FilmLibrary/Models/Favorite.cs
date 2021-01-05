079f452f8369dc130538480ec6de182fb24deba2
Khornya <marine.khornya@gmail.com>
2021-01-05 23:00:08 +0100

    added collection film view (WIP : stretch items)

--- FilmLibrary/FilmLibrary/Models/Favorite.cs
+++ FilmLibrary/FilmLibrary/Models/Favorite.cs
@@ -12,11 +12,13 @@ namespace FilmLibrary.Models
     public class Favorite : ObservableObject
     {
         private Film _Film;
-
-        public Film Film {
-            get => this._Film;
-            set => this._Film = value;
-        }
+        private int _Note;
+        private string _Comment;
+        private bool _Seen;
+        public Film Film { get => this._Film; set => this.SetProperty(nameof(this.Film), ref this._Film, value); }
+        public int Note { get => _Note; set => this.SetProperty(nameof(this.Note), ref this._Note, value); }
+        public string Comment { get => _Comment; set => this.SetProperty(nameof(this.Comment), ref this._Comment, value); }
+        public bool Seen { get => _Seen; set => this.SetProperty(nameof(this.Seen), ref this._Seen, value); }
 
         public Favorite(Film film)
         {
